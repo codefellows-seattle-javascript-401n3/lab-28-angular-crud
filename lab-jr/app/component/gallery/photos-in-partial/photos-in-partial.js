455a0b7de582f547987dc6d61615a12ca1495101
Frank Iriarte <jurrish@gmail.com>
2017-03-06 15:29:04 -0800

    finished up the photoService...just need deletePhoto method to pass to controller

---
+++ lab-jr/app/component/gallery/photos-in-partial/photos-in-partial.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  template: require('./photos-in-partial.html'),
+  controller: ['$log', 'photoService', PhotosInPartialController],
+  controllerAs: 'photosInPartialCtrl',
+  bindings: {
+    pic: '<'
+  }
+};
+
+function PhotosInPartialController($log, photoService){
+  $log.debug('ThumbnailController loaded');
+
+  this.deletePic = function(){
+    $log.debug('thumbnailCtrl.deletePic()');
+
+    //write function inside photoService to delete a photo
+  }
+}
