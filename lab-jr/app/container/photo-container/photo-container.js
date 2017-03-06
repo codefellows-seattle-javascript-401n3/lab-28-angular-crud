efed8aeb0533bf426dc0d9f5dc26decc2712fc47
Frank Iriarte <jurrish@gmail.com>
2017-03-06 15:29:51 -0800

    test

--- lab-jr/app/container/photo-container/photo-container.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-//contains logic to hold photos for the instances of uploaded pics inside a gallery
--- lab-jr/app/service/photo-service.js
+++ lab-jr/app/service/photo-service.js
@@ -1,1 +1,42 @@
 'use strict';
+
+module.exports = ['$q', '$log', '$http', 'Upload', 'authService', photoService];
+
+function photoService($q, $log, $http, Upload, authService){
+  $log.debug('inside photoService for uploads');
+  let service = {};
+
+  service.uploadPic = function(galleryId, picProps) {
+    $log.debug('photoservice.uploadPic()');
+
+    return authService.getToken()
+    .then(token => {
+      let url = `${__API_URL__}/api/gallery/${galleryId._id}/pic`;
+      let headers = {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      };
+      return Upload.upload({
+        url,
+        headers,
+        method: 'POST',
+        data: {
+          name: picProps.name,
+          desc: picProps.desc,
+          file: picProps.file,
+        },
+      });
+    })
+    .then(res => {
+      galleryId.pics.unshift(res.data);
+      console.log(res.data, ' = res.data');
+      return res.data;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  return service;
+}
