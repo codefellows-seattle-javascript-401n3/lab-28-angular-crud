63d24f479679aa91dafd76d3418136747bfc36c8
Frank Iriarte <jurrish@gmail.com>
2017-03-15 18:43:02 -0700

    1 expect working

--- lab-jr/test/edit-gallery-component-test.js
+++ lab-jr/test/edit-gallery-component-test.js
@@ -4,55 +4,59 @@ describe('edit-gallery-component', function(){
   beforeEach(() => {
     angular.mock.module('cfgram');
     angular.mock.inject(($rootScope, $window, $componentController, $httpBackend, authService) => {
-
       this.$rootScope = $rootScope;
-      this.$window = $window;
       this.$componentController = $componentController;
       this.$httpBackend = $httpBackend;
       this.authService = authService;
     });
-
-    this.testToken = 'testToken';
-    this.$window.localStorage.setItem('token', this.testToken);
-
   });
 
-  afterEach(() => {
-    this.$window.localStorage.removeItem('token');
-  });
+  it('should have the mock bindings', () => {
+    let mockBindings = {
+      gallery: {
+        name: 'test gallery name',
+        desc: 'test gallery desc'
+      }
+  };
+
+  let editGalleryCtrl = this.$componentController('editGallery', null, mockBindings);
+  expect(editGalleryCtrl.gallery.name).toBe('test gallery name');
+  expect(editGalleryCtrl.gallery.desc).toBe('test gallery desc');
+
+  this.$rootScope.$apply();
+});
 
   describe('editGallery.updateGallery()', () => {
     it('should use updateGallery to update Gallery object', () => {
       let url = 'http://localhost:3000/api/gallery/12345';
-
-      let updatedGallery = {
-        _id: '12345',
-        name: 'updated name',
-        desc: 'updated desc'
-      };
-
       let headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json;charset=utf-8',
         Authorization: `Bearer ${this.testToken}`
       };
 
-      this.$httpBackend.expectPUT(url, updatedGallery, headers).respond(200);
+      this.$httpBackend.expectPUT(url, {
+      _id: '12345',
+      name: 'updated name',
+      desc: 'updated description'
+      }, headers).respond(200);
 
       let mockBindings = {
         gallery: {
           _id: '12345',
           name: 'updated name',
-          desc: 'updated desc'
-        }
+          desc: 'updated description'
+        },
       };
-
-
-      let editGalleryCtrl = this.$componentController('galleryService', null, mockBindings);
-      console.log('$componentController for editGallery:', editGalleryCtrl.updateGallery);
+      let editGalleryCtrl = this.$componentController('editGallery', null, mockBindings);
       editGalleryCtrl.gallery.name = 'updated name';
-      editGalleryCtrl.gallery.desc = 'updated desc';
+      editGalleryCtrl.gallery.desc = 'updated description';
       editGalleryCtrl.updateGallery();
+      // let updatedGallery = {
+      //   _id: '12345',
+      //   name: 'updated name',
+      //   desc: 'updated desc'
+      // };
 
       this.$httpBackend.flush();
       this.$rootScope.$apply();
